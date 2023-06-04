f545be937729c18e0fb9e31cfcf01ba6a79493be
linxu <linxu@linxu.com>
2023-06-04 23:38:56 +0800

    1

--- sidebars.js
+++ sidebars.js
@@ -1,19 +1,5 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  skill: [
-    'skill/introduction',
-    {
-      label: 'Docusaurus 主题魔改',
-      type: 'category',
-      link: {
-        type: 'doc',
-        id: 'skill/docusaurus/docusaurus-guides'
-      },
-      items: [
-        'skill/docusaurus/docusaurus-config',
-      ],
-    },
-  ],
 }
 
 module.exports = sidebars
